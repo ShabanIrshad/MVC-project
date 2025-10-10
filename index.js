9ee9744e401ea1887fc59d9f2eae078fd945600a
Shaban Irshad <shabanirshad408@gmail.com>
2025-10-10 18:52:13 +0530

    first commit of project

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+import ejsLayouts from 'express-ejs-layouts'
+import {home} from './src/controller/homeController.js';
+const app=express();
+
+const port=3100;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(path.resolve(), 'src','views'));
+app.use(ejsLayouts);
+// app.set();
+
+app.get('/',home);
+
+app.use(express.static('src/views'));
+app.set('layout', 'layout');
+
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(err);
+    }
+    console.log('App is listening on port : ',port);
+})
---
+++ src/controller/homeController.js
@@ -0,0 +1,4 @@
+function home(req,res){
+   return res.render('home');
+}
+export {home};
